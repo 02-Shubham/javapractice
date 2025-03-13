8b826186c130ae4578c5e50d06ace26216b8fb6d
ShubhamSahu <shubhamsahu60582@gmail.com>
2025-03-13 12:43:31 +0530

    arithmetic done

---
+++ arithmetic.java
@@ -0,0 +1,9 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        //Order of operation  [P_E_M_D_A_S]
+         
+        double result =3+4*(7-5)/2.0;
+        
+        System.out.println(result);
+    }
+}
